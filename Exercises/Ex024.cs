e8da47fcf807ba12ffb8a1d79c4a61180ada1a18
kaiwang23 <wangkai.wk@outlook.com>
2026-01-06 01:23:48 +0800

    24-26

--- Exercises/Ex024.cs
+++ Exercises/Ex024.cs
@@ -9,109 +9,53 @@ using System.Threading.Tasks;
 
 namespace ProgrammingExercises100.Exercises
 {
-    internal class Ex024 : IExercise
+    internal class Ex025 : IExercise
     {
         public void Run()
         {
-            Console.WriteLine("--- 练习 024: 通过运算符重载实现矩阵相加 ---");
+            Console.WriteLine("--- 练习 025: 静态构造函数练习 ---");
             // 题目描述
-            string line = "实现一个类Matrix：能够完成两个矩阵的相加，这个类用二维数组存储整数值，包含两个属性行数和列数，两个属性在构造器中初始化。任务是：添加一个indexer索引器，实现访问和修改元素，通过matrix[row,column]语法；重载+运算符，来实现矩阵加法";
+            string line = "一个日志系统需要通过为每条消息分配唯一 ID 来跟踪它已处理的消息数量。Logger 类使用一个静态字段（_logCounter）来统计日志条目数量。你的任务是：实现一个静态构造函数，将 _logCounter 初始化为 1。在你的解决方案中使用提供的静态字段（_logCounter）。实现静态方法 Log，用于输出每条消息及其用括号括起来的 ID（例如 [i] Message）。每次调用 Log 时，应递增计数器，以便下一条消息获得下一个 ID。";
             Console.WriteLine(line);
 
             // 准备一些测试数据
-
-            Matrix m1 = new Matrix(3,4);
-            Matrix m2 = new Matrix(3,4);
-
-            for (int i = 0; i < 3; i++)
-            {
-                for (int j = 0; j < 4; j++)
-                {
-                    m1[i, j] = i;
-                    m2[i, j] = j;
-                }
-            }
-            //0000
-            //1111
-            //2222
-            
-            //0123
-            //0123
-            //0123
+            Logger.Log("test1");
+            Logger.Log("test2");
+            Logger.Log("test3");
 
 
             // 调用你的逻辑方法
 
-            Matrix m3 = m1 + m2;
+
 
             // 输出结果
-            for (int i = 0; i < 3; i++)
-            {
-                for (int j = 0; j < 4; j++)
-                {
-                    Console.Write(m3[i,j]);
-                }
-                Console.WriteLine();
-            }
-            //0123
-            //1234
-            //2345
+
 
         }
 
 
         //题目知识：
-        // 1.自定义数据解构的方法：定义类，字段，构造器，索引器，数据解构特殊的操作符
-        // 2.int[,]表示二维数组，构造器的意义是在创建实例的时候，传入什么参数，需要怎么把传入的参数和字段联系起来
-        // 3.索引器使用this关键字（可以理解为作为方法名），里面包含get和set访问器，get表示输入[]参数后获得值的规则，set表示怎么从外界给里面的数据赋值，注意有一个value可以使用。
-        // 4.字段的readonly表示 局部变量的引用本身不可变（不能重新赋值），即_data永远指向一个数组对象，而不能重新指向另一个对象，而对象内部的值是不受这个readonly管理的，所以索引器中可以set值
-        // 5.方法重载overload 和 方法重写override是完全不同的概念，重载是方法签名不一致但是方法名一致的时候选择哪个具体方法的情况，而重写是在继承链上，拥有同样的方法签名。重载不需要有特殊的修饰符，而重写一定是abstract或virtual与override搭配使用。重载是编译时绑定（静态绑定），是静态多态，重写是运行时绑定（动态绑定），是动态多态
+        // 1.静态类中可以有静态构造函数，在不需要创建实例的情况下，就能进行一些初始化操作，在CLR加载这个静态类的时候，静态构造函数就执行了，如果程序不执行这个类名相关的代码，就不会执行静态构造函数
+
 
     }
-    public class Matrix
+    public static class Logger
     {
-        private readonly int[,] _data;
-        private readonly int _rowsCount;
-        private readonly int _columnsCount;
+        private static int _logCounter;
 
-        public Matrix(int rowsCount, int columnsCount)
+        static Logger()
         {
-            _rowsCount = rowsCount;
-            _columnsCount = columnsCount;
-            _data = new int[_rowsCount, _columnsCount];   //这里初始化后，_data只能指向这个对象，而不能指向别的对象了
+            _logCounter = 1;
         }
 
-        public int this[int row, int column]
+        public static void Log(string message)  //Log方法，输出每条消息时附带唯一ID，且每次ID递增
         {
-            get
-            {
-                return _data[row, column];
-            }
-            set
-            {
-                _data[row, column] = value;
-            }
+            Console.WriteLine($"[{_logCounter}] {message}");
+            _logCounter++;
         }
-
-        public static Matrix operator +(Matrix a, Matrix b)
+        public static void ResetCounter()
         {
-            if (a._rowsCount == b._rowsCount && a._columnsCount == b._columnsCount)
-            {
-                Matrix result = new Matrix(a._rowsCount, a._columnsCount);
-                for (int i = 0; i < a._rowsCount; i++)
-                {
-                    for (int j = 0; j < a._columnsCount; j++)
-                    {
-                       result[i,j] = a[i,j] + b[i,j];
-                    }
-                    
-                }
-                return result;
-            }
-            else
-            {
-                throw new InvalidOperationException();
-            }
+            _logCounter = 1;
         }
     }
 
---
+++ Exercises/Ex026.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Net.Mail;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingExercises100.Exercises
+{
+    internal class Ex026 : IExercise
+    {
+        public void Run()
+        {
+            Console.WriteLine("--- 练习 026: 自定义转换运算符 ---");
+            // 题目描述
+            string line = "任务：实现从 TimeDuration 到 TimeSpan 的隐式转换，以便在时间 API 中自动使用。实现从 TimeSpan 到 TimeDuration 的显式转换，以便允许转换回来，并截断小数秒（TimeSpan 允许小数秒，但 TimeDuration 不允许）。";
+            Console.WriteLine(line);
+
+            // 准备一些测试数据
+
+            TimeDuration timeDuration1 = new TimeDuration(5);
+            TimeSpan timeSpan1 = timeDuration1;
+
+            TimeSpan timeSpan2 = TimeSpan.FromSeconds(1.5);
+            TimeDuration timeDuration2 = (TimeDuration)timeSpan2;
+
+            // 调用你的逻辑方法
+
+
+
+            // 输出结果
+            Console.WriteLine($"TimeDuration1:{timeDuration1.Seconds}");
+            Console.WriteLine($"TimeSpan1:{timeSpan1}");
+            Console.WriteLine($"TimeSpan2:{timeSpan2}");
+            Console.WriteLine($"TimeDuration2:{timeDuration2.Seconds}");
+
+        }
+
+
+        //题目知识：
+        // 1.用户自定义转换运算符有自己专门的语法
+
+        // 2.从 A 到 B 的隐式转换(implicit)：也就是说 B 引用变量可以直接接受一个 A 类型的值：B b = a;
+        // public static implicit operator B(A a) => B接受A值的操作;
+
+        // 3.从 B 到 A 的显式转换(emplicit)：A a = (A)b;
+        // public static explicit operator A(B b) => A接受B值的操作;
+
+
+    }
+    public class TimeDuration
+    {
+        public int Seconds { get; }
+        public TimeDuration(int seconds)
+        {
+            Seconds = seconds;
+        }
+
+        public static implicit operator TimeSpan(TimeDuration timeDuration) => TimeSpan.FromSeconds(timeDuration.Seconds);
+        public static explicit operator TimeDuration(TimeSpan timeSpan) => new TimeDuration((int)timeSpan.TotalSeconds);
+    }
+}
